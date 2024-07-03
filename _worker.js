cdf12a9f81d195e096ba11a6cb3524190a3bac83
yanghuilong <601125163@qq.com>
2024-07-03 13:34:13 +0800

    Update _worker.js
    删除uuid

--- _worker.js
+++ _worker.js
@@ -5,19 +5,9 @@
 
 import { connect } from 'cloudflare:sockets';
 
-function generateUUID() {
-var d = new Date().getTime();
-var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-  var r = (d + Math.random()*16)%16 | 0;
-  d = Math.floor(d/16);
-  return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-});
-return uuid;
-};
-
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = generateUUID();
+let userID = '';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
